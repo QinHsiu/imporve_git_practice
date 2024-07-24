7f849b127d7f65426737c8eae8ff7d8687441a0e
Yixuan <yd271@georgetown.edu>
2024-07-24 11:00:58 +0800

    111

--- eval.py
+++ eval.py
@@ -86,7 +86,7 @@ class QuestionDatasetEvaluator:
             
 
     def load_dataset(self, id):
-        print("bye!")
+        print("112!")
         eval_info={
             "id":id,
             'info':self._id2evalinfo[id],
